ffa4ca85739feba27776de150de257497483e187
Michael Meisel <mrmeisel@gmail.com>
2017-07-17 22:28:35 -0700

    Mostly failed attempt to work with H-Bridge

--- timer.ino
+++ timer.ino
@@ -3,26 +3,32 @@
 
 // Pins
 #define PIN_ANALOG_IN A0
-#define PIN_ANALOG_OUT 3
+#define PIN_OUTPUT 3
+#define PIN_DIR1 7
+#define PIN_DIR2 6
 #define PIN_BELL LED_BUILTIN
 
+// Directions
+#define DIR_STOP 0
+#define DIR_REVERSE -1
+#define DIR_FORWARD 1
+
 // Slider properties
-#define ZERO_POSITION 8.0
-#define MAX_POSITION 1023.0
-#define FUDGE_FACTOR 2.0
+// Slider positions come from analogRead, so the range is 0 to 1023
+#define FUDGE_FACTOR 2
+#define OFF_STOP -1
 
-// Current slider position
-double position;
+// Allowable positions where the motor will create virtual detentes.
+// The first position will always be the zero position (where the alarm will sound).
+// Position zero is reserved for "off", so the first position should be at least 2 * FUDGE_FACTOR.
+const int STOPS[] = {
+    32, 96, 160, 224, 288, 352, 416, 480, 544, 608, 672, 736, 800, 864, 928, 992
+};
 
-// Currently desired slider position
-double setPoint;
+const int NUM_STOPS = sizeof(STOPS) / sizeof(int);
 
-// Output of PID algorithm
-double pidOutput;
-
-// See http://playground.arduino.cc/Code/PIDLibrary
-// P_ON_M (proportional on measurement) mode is better for linear slide pots
-PID pid(&position, &pidOutput, &setPoint, 2, 5, 1, P_ON_M, DIRECT);
+int currentPosition;
+int expectedPosition;
 
 bool isRunning;
 volatile long msRemaining;
@@ -38,25 +44,109 @@ void tick() {
     }
 }
 
-// Linear with maximum of one minute
-long positionToMs(double position) {
-    return round(60000.0 * max(0, position - ZERO_POSITION) / (MAX_POSITION - ZERO_POSITION));
+void setDirection(int direction) {
+    Serial.print("Setting direction to ");
+    Serial.print(direction);
+    Serial.print("\n");
+
+    switch (direction) {
+        case DIR_STOP:
+            digitalWrite(PIN_DIR1, LOW);
+            digitalWrite(PIN_DIR2, LOW);
+            digitalWrite(PIN_OUTPUT, LOW);
+            break;
+        case DIR_REVERSE:
+            digitalWrite(PIN_DIR1, LOW);
+            digitalWrite(PIN_DIR2, HIGH);
+            digitalWrite(PIN_OUTPUT, HIGH);
+            break;
+        case DIR_FORWARD:
+            digitalWrite(PIN_DIR1, HIGH);
+            digitalWrite(PIN_DIR2, LOW);
+            digitalWrite(PIN_OUTPUT, HIGH);
+            break;
+    }
 }
 
-double msToPosition(long ms) {
-    return ZERO_POSITION + ms * (MAX_POSITION - ZERO_POSITION) / 60000.0;
+/*
+int whichStop(int position) {
+    if (position <= STOPS[0] + FUDGE_FACTOR) {
+        return STOPS[0];
+    }
+
+    if (position >= STOPS[NUM_STOPS - 1] - FUDGE_FACTOR) {
+        return STOPS[NUM_STOPS - 1];
+    }
+
+    for (int i = 1; i < NUM_STOPS - 1; i++) {
+        int stop = STOPS[i];
+
+        if (position >= stop - FUDGE_FACTOR && position <= stop + FUDGE_FACTOR) {
+            return stop;
+        }
+    }
+
+    // Not at any stop.
+    return NO_STOP;
+}
+*/
+
+int stopBefore(int position) {
+    if (position < STOPS[0] - FUDGE_FACTOR) {
+        return OFF_STOP;
+    }
+
+    for (int i = 1; i < NUM_STOPS; i++) {
+        if (position < STOPS[i] - FUDGE_FACTOR) {
+            return i - 1;
+        }
+    }
+
+    return NUM_STOPS - 1;
+}
+
+int nearestStop(int position) {
+    int before = stopBefore(position);
+
+    if (before == NUM_STOPS - 1) {
+        return NUM_STOPS - 1;
+    }
+
+    int after = before + 1;
+    int beforeEnd = (before == OFF_STOP ? 0 : STOPS[before]) + FUDGE_FACTOR;
+    int distance = STOPS[after] - beforeEnd - 2 * FUDGE_FACTOR;
+
+    return position < beforeEnd + distance / 2 ? before : after;
+}
+
+// Linear with maximum of 30 seconds
+long positionToMs(int position) {
+    int firstPosition = STOPS[0];
+    int lastPosition = STOPS[NUM_STOPS - 1];
+
+    return lround(30000.0 * max(0, position - firstPosition) / (lastPosition - firstPosition));
+}
+
+int msToPosition(long ms) {
+    int firstPosition = STOPS[0];
+    int lastPosition = STOPS[NUM_STOPS - 1];
+
+    return (int) lround(firstPosition + ms * (lastPosition - firstPosition) / 30000.0);
 }
 
 
 
 void setup() {
+    pinMode(PIN_OUTPUT, OUTPUT);
+    pinMode(PIN_DIR1, OUTPUT);
+    pinMode(PIN_DIR2, OUTPUT);
+    pinMode(PIN_BELL, OUTPUT);
+
     digitalWrite(PIN_BELL, LOW);
 
-    position = analogRead(PIN_ANALOG_IN);
-    setPoint = position;    // Don't move the slider on startup
-    msRemaining = positionToMs(position);
-
-    pid.SetMode(AUTOMATIC);
+    currentPosition = analogRead(PIN_ANALOG_IN);
+    expectedPosition = nearestStop(currentPosition);    // Don't move the slider on startup
+    msRemaining = positionToMs(expectedPosition);
 
     Timer1.initialize(1000000); // 1 second in microseconds
     Timer1.attachInterrupt(tick);
@@ -66,60 +156,75 @@ void setup() {
 }
 
 void loop() {
-    position = analogRead(PIN_ANALOG_IN);
+    currentPosition = analogRead(PIN_ANALOG_IN);
 
-    Serial.print("position = ");
-    Serial.print(position);
+    Serial.print("currentPosition = ");
+    Serial.print(currentPosition);
     Serial.print(", msRemaining = ");
     Serial.print(msRemaining);
     Serial.print("\n");
 
-    if (position <= ZERO_POSITION + FUDGE_FACTOR) {
-        // Ring the bell based on whether we are at zero (DING!) or below zero (off).
-        bool isInOffPosition = position < ZERO_POSITION - FUDGE_FACTOR;
+    int nearest = nearestStop(currentPosition);
+    int nearestPosition = nearest == OFF_STOP ? 0 : STOPS[nearest];
+    long curMsRemaining;
 
-        digitalWrite(PIN_BELL, isInOffPosition ? LOW : HIGH);
-        Timer1.stop();
-        isRunning = false;
-
-        if (!isInOffPosition) {
-            Serial.print("DING!\n");
+    if (nearestPosition <= STOPS[0]) {
+        digitalWrite(PIN_BELL, nearest == OFF_STOP ? LOW : HIGH);
+        if (isRunning) {
+            Timer1.stop();
+            isRunning = false;
         }
     }
-    else {
-        bool setMsRemaining = false;
-        long curMsRemaining;
-
+    else if (nearestPosition != expectedPosition) {
+        // Slider was moved to a value (other than zero or off), set msRemaining based on this
         if (!isRunning) {
+            digitalWrite(PIN_BELL, LOW);
             Timer1.start();
             isRunning = true;
         }
 
-        // If the position is off by more than the fudge factor, assume the slider was moved and
-        // reset msRemaining based on the position.
-        if (abs(position - setPoint) > FUDGE_FACTOR) {
-            setMsRemaining = true;
-            curMsRemaining = positionToMs(position);
-        }
+        expectedPosition = nearestPosition;
+        curMsRemaining = positionToMs(nearestPosition);
 
         noInterrupts();
-        if (setMsRemaining) {
-            msRemaining = curMsRemaining;
-        }
-        else {
-            curMsRemaining = msRemaining;
-        }
+        msRemaining = curMsRemaining;
+        interrupts();
+    }
+    else {
+        // Normal operation
+        noInterrupts();
+        curMsRemaining = msRemaining;
         interrupts();
 
-        // Move the slider towards zero based on seconds remaining
-        setPoint = msToPosition(curMsRemaining);
+        // Find the new expectedPosition based on ms remaining
+        int newNearest = nearestStop(msToPosition(curMsRemaining));
+        int newExpectedPosition = newNearest == 0 ? 0 : STOPS[newNearest];
 
-        Serial.print("setPoint = ");
-        Serial.print(setPoint);
-        Serial.print("\n");
+        // If it changed, move the slider
+        if (expectedPosition == newExpectedPosition) {
+            setDirection(DIR_STOP);
+        }
+        else {
+            Serial.print("newExpectedPosition = ");
+            Serial.print(newExpectedPosition);
+            Serial.print("\n");
 
-        pid.Compute();
-        analogWrite(PIN_ANALOG_OUT, pidOutput);
+            setDirection(expectedPosition > newExpectedPosition ? DIR_REVERSE : DIR_FORWARD);
+            expectedPosition = newExpectedPosition;
+        }
     }
+
+    // If the position is off by more than the fudge factor, create the virtual detentes with the
+    // motor.
+    if (currentPosition < expectedPosition - FUDGE_FACTOR) {
+        setDirection(DIR_FORWARD);
+    }
+    else if (currentPosition > expectedPosition + FUDGE_FACTOR) {
+        setDirection(DIR_REVERSE);
+    }
+    else {
+        setDirection(DIR_STOP);
+    }
+
     delay(250);
 }
