6233b9f413a023d713959d957a296044249c4017
Michael Meisel <mrmeisel@gmail.com>
2017-07-18 18:15:19 -0700

    Reliability and efficiency improvements

--- timer.ino
+++ timer.ino
@@ -15,6 +15,7 @@
 // Slider properties
 // Slider positions come from analogRead, so the range is 0 to 1023
 #define FUDGE_FACTOR 6
+#define RESET_THRESHOLD_MS 40000
 
 // Stops where the motor will create virtual detentes.
 // The first stop should always be the "OFF" position. The second stop should always be the zero
@@ -51,7 +52,6 @@ const int NUM_STOPS = sizeof(STOPS) / sizeof(Stop);
 
 int currentPosition;
 int currentStop;
-bool isMoving;
 unsigned long deadline;
 
 
@@ -110,62 +110,63 @@ void setup() {
 
     currentPosition = analogRead(PIN_ANALOG_IN);
     currentStop = findNearestStop(currentPosition);    // Don't move the slider on startup
-
     deadline = millis() + STOPS[currentStop].ms;
+
     Serial.begin(9600);
 }
 
 void loop() {
     currentPosition = analogRead(PIN_ANALOG_IN);
 
-    if (!isMoving) {
-        unsigned long now = millis();
-        int nearestStop = findNearestStop(currentPosition);
-        bool stopChanged = false;
+    unsigned long now = millis();
+    long remaining = (long) (deadline - now);
+    bool stopChanged = false;
 
-        if (nearestStop != currentStop) {
-            // Slider was moved by a human
-            deadline = now + STOPS[nearestStop].ms;
-            currentStop = nearestStop;
-            stopChanged = true;
-        }
-        else if (currentStop > 1 && (long) (deadline - now) <= STOPS[currentStop - 1].ms) {
+    // Check if the slider is in the expected position. If not, a human must have moved it.
+    if (currentPosition < STOPS[currentStop].position - FUDGE_FACTOR ||
+        currentPosition > STOPS[min(currentStop + 1, NUM_STOPS - 1)].position + FUDGE_FACTOR)
+    {
+        int nearestStop = findNearestStop(currentPosition);
+
+        deadline = now + STOPS[nearestStop].ms;
+        currentStop = nearestStop;
+        stopChanged = true;
+    }
+    else if (currentStop > 1) {
+        // Normal operation (not off or ringing)
+        if (remaining <= STOPS[currentStop - 1].ms) {
             // Time to move the slider to the next stop down
             currentStop--;
             stopChanged = true;
-            isMoving = true;
-        }
-
-        if (stopChanged) {
-            // Enable or disable the bell based on whether we're at the zero stop (stop 1)
-            digitalWrite(PIN_BELL, currentStop == 1 ? HIGH : LOW);
-        }
-
-        if (Serial.available() > 0) {
-            Serial.read();  // Doesn't matter what it is, just output state on any input
-            Serial.print("timeRemaining = ");
-            Serial.print((long) (deadline - now));
-            Serial.print(", currentStop = ");
-            Serial.print(currentStop);
-            Serial.print(", currentPosition = ");
-            Serial.print(currentPosition);
-            Serial.print(", nearestStop = ");
-            Serial.print(nearestStop);
-            Serial.print("\n");
         }
     }
 
-    // Normal operation, keep the slider in the desired position
+    if (stopChanged) {
+        // Enable or disable the bell based on whether we're at the zero stop (stop 1)
+        digitalWrite(PIN_BELL, currentStop == 1 ? HIGH : LOW);
+    }
+
+    if (Serial.available() > 0) {
+        Serial.read();  // Doesn't matter what it is, just output state on any input
+        Serial.print("remaining = ");
+        Serial.print(remaining);
+        Serial.print(", currentStop = ");
+        Serial.print(currentStop);
+        Serial.print(", currentPosition = ");
+        Serial.print(currentPosition);
+        Serial.print("\n");
+    }
+
+    // Keep the slider in the desired position
     int expectedPosition = STOPS[currentStop].position;
 
-    if (currentPosition < expectedPosition - FUDGE_FACTOR) {
+    if (currentPosition <= expectedPosition - FUDGE_FACTOR) {
         setDirection(DIR_FORWARD);
     }
-    else if (currentPosition > expectedPosition + FUDGE_FACTOR) {
+    else if (currentPosition >= expectedPosition + FUDGE_FACTOR) {
         setDirection(DIR_REVERSE);
     }
     else {
         setDirection(DIR_STOP);
-        isMoving = false;
     }
 }
